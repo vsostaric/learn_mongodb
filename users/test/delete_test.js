c18cf7709fe2856c49719517139620634bfea323
Vladimir Sostaric <vladimirsostaric3390@gmail.com>
2017-03-12 11:37:39 +0100

    add delete test

---
+++ users/test/delete_test.js
@@ -0,0 +1,69 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Deleting a user', () => {
+
+  let joe;
+
+  beforeEach((done) => {
+
+    joe = new User({name: 'Joe'});
+    joe.save().then(() => {
+      done();
+    });
+
+  });
+
+  it('model instance remove', (done) => {
+
+    joe.remove()
+      .then(() => {
+        return User.findOne({name: 'Joe'});
+      })
+      .then((user) => {
+      assert(user === null);
+      done();
+    });
+
+  });
+
+  it('class method remove', (done) => {
+
+    User.remove({name: 'Joe'})
+      .then(() => {
+        return User.findOne({name: 'Joe'});
+      })
+      .then((user) => {
+      assert(user === null);
+      done();
+    });
+
+  });
+
+  it('class method findOneAndRemove', (done) => {
+
+    User.findOneAndRemove({name: 'Joe'})
+      .then(() => {
+        return User.findOne({name: 'Joe'});
+      })
+      .then((user) => {
+      assert(user === null);
+      done();
+    });
+
+  });
+
+  it('class method findByIdAndRemove', (done) => {
+
+    User.findByIdAndRemove(joe)
+      .then(() => {
+        return User.findOne({name: 'Joe'});
+      })
+      .then((user) => {
+      assert(user === null);
+      done();
+    });
+
+  });
+
+});
