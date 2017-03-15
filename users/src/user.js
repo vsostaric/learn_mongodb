68dd9319751b9ebd190511a3ccd39926b51e94b5
Vladimir Sostaric <vladimirsostaric3390@gmail.com>
2017-03-15 11:00:34 +0100

    Add middleware and test

--- users/src/user.js
+++ users/src/user.js
@@ -26,6 +26,12 @@ userSchema.virtual('postCount').get(function() {
   return this.posts.length;
 });
 
+userSchema.pre('remove', function(next) {
+  const BlogPost = mongoose.model('blogPost');
+  BlogPost.remove({_id: {$in: this.blogPosts}})
+  .then(() => next());
+});
+
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
